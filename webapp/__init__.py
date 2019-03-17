16b224204146ebc8483a86fa95519b2903fb07f7
genek771 <genek771@yandex.ru>
2019-03-17 22:53:30 +0300

    First commit

---
+++ webapp/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
+
+
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
+
+
+def create_app():
+	app = Flask(__name__)
+	app.config.from_pyfile('config.py')
+
+	@app.route('/')
+	def index():
+		title = "Новости Python"
+		weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+		news = get_python_news()
+		return render_template('index.html', page_title=title, weather=weather, news_list=news)
+
+	return app
